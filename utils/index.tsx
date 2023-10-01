298ebbeff5f174716708af09e02592090d055f94
najah <najahaboobacker79@gmail.com>
2023-10-01 17:55:20 +0530

    api

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,74 +1,68 @@
+import axios from 'axios';
 import { CarProps } from "@/types";
 import { FilterProps } from "@/types";
 
-export async function fetchCars(filters:FilterProps) {
-  const {manufacturer,year,model,limit,fuel}=filters;
-    const headers={'X-RapidAPI-Key': '1126204450mshd83ba7fa13b491dp1ebe3djsnae766e33565f',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'}
-    const response = await fetch(
-      `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-      {
-        headers: headers,
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
+  const headers = {
+    'X-RapidAPI-Key': '1126204450mshd83ba7fa13b491dp1ebe3djsnae766e33565f',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  };
+
+  try {
+    const response = await axios.get('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+      headers: headers,
+      params: {
+        make: manufacturer,
+        year: year,
+        model: model,
+        limit: limit,
+        fuel_type: fuel
       }
-    );
-        const result =await  response.json();
-        console.log(result);
-return result;
-    
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching cars:", error);
+    throw error;
+  }
 }
-export const generateCarImageUrl =(car:CarProps ,angle?:string)=>{
-  const url=new URL('https://cdn.imagin.studio/getimage'); 
-  const {make , year, model}=car
-  url.searchParams.append('customer','hrjavascript-mastery');
-  url.searchParams.append('make',make);
-  url.searchParams.append('modelFamily',model.split(' ')[0]);
-  url.searchParams.append('zoomType','fullscreen');
-  url.searchParams.append('modelYear',`${year}`);
-  url.searchParams.append('angle',`${angle}`);
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  const { make, year, model } = car;
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  if (angle) {
+    url.searchParams.append('angle', angle);
+  }
   return `${url}`;
-   
-
 }
-export const calculateCarRent = (city_mpg: number, year: number) => {
-  const basePricePerDay = 50; // Base rental price per day in dollars
-  const mileageFactor = 0.1; // Additional rate per mile driven
-  const ageFactor = 0.05; // Additional rate per year of vehicle age
 
-  // Calculate additional rate based on mileage and age
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50;
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
   const mileageRate = city_mpg * mileageFactor;
   const ageRate = (new Date().getFullYear() - year) * ageFactor;
-
-  // Calculate total rental rate per day
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-
   return rentalRatePerDay.toFixed(0);
 };
 
 export const updateSearchParams = (type: string, value: string) => {
-  // Get the current URL search params
   const searchParams = new URLSearchParams(window.location.search);
-
-  // Set the specified search parameter to the given value
   searchParams.set(type, value);
-
-  // Set the specified search parameter to the given value
   const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
-
   return newPathname;
 };
 
 export const deleteSearchParams = (type: string) => {
-  // Set the specified search parameter to the given value
   const newSearchParams = new URLSearchParams(window.location.search);
-
-  // Delete the specified search parameter
   newSearchParams.delete(type.toLocaleLowerCase());
-
-  // Construct the updated URL pathname with the deleted search parameter
   const newPathname = `${window.location.pathname}?${newSearchParams.toString()}`;
-
   return newPathname;
 };
-
-
-    
